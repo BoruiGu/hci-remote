140757cbe3cb8cceb794787d39a4c134077979fa
BoruiGu <alax919@gmail.com>
2015-11-23 19:51:19 -0500

    Include jQuery, Bootstrap, Angular; add playground

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(multer()); // for parsing multipart/form-data
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 2525;
 
-app.get('/hello', function (req, res) {
-    res.send('hello world');
-});
-
 app.use(express.static(__dirname + '/src'));
 
 app.listen(port, ip);
+
+app.get('/api/hello/:val', function (req, res) {	
+    res.send('Hello from ' + req.params.val + '!');
+});
 
 //connection.end();
---
+++ src/app.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("UCBApp", ['ngTouch', 'ngAnimate']);
+app.controller("UCBCtrl", function ($scope, $http) {
+    $scope.angular_hello = "Hello from Angular!";
+	var val = "Node";
+    $http.get("/api/hello/" + val)
+    .success(function (response) {
+        $scope.node_hello = response;
+    })
+    .error(function (data, status) {
+        $scope.node_hello = "call to node.js failed!";
+        console.log(data);
+        console.log(status);
+    });
+});
