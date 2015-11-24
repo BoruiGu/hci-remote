7e96b1f47a76425bd914cb63afda768f7ea4ef8c
BoruiGu <alax919@gmail.com>
2015-11-24 10:03:47 -0500

    Create header

---
+++ src/controller/home.js
@@ -0,0 +1,17 @@
+﻿app.controller("HomeCtrl", function ($scope, $http, $rootScope) {
+    $scope.angular_hello = "Hello from Angular!";
+	var val = "Node";
+    $http.get("/api/hello/" + val)
+    .success(function (response) {
+        $scope.node_hello = response;
+    })
+    .error(function (data, status) {
+        $scope.node_hello = "call to node.js failed!";
+        console.log(data);
+        console.log(status);
+    });
+
+    // this line will be removed after login screen created
+    $rootScope.username = 'Sharon';
+    $scope.username = $rootScope.username;
+});
