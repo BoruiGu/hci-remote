1ccae33eb0d7192d4a9f5c5da0e7d09a102715c9
BoruiGu <alax919@gmail.com>
2015-12-05 11:30:34 -0500

    Add level chart

--- src/partial/my_level/my_level.js
+++ src/partial/my_level/my_level.js
@@ -3,4 +3,19 @@
     .success(function (response) {
         $scope.level_info = response;
     });
+
+    $scope.username = $rootScope.username;
+
+    $scope.user_chart = [
+        { username: 'Freudia', level: 5 },
+        { username: 'Ingrid', level: 4 },
+        { username: 'Cielo', level: 4 },
+        { username: 'Senna', level: 3 },
+        { username: 'Goma', level: 3 },
+        { username: 'Shermie', level: 3 },
+        { username: 'Saiya', level: 3 },
+        { username: '###', level: 2 },
+        { username: 'Millia', level: 2 },
+        { username: 'Zenon', level: 2 },
+    ];
 });
