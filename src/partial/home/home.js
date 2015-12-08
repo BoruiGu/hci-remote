4b169fc4695300f7d40b8215cf8706510557ddd3
BoruiGu <alax919@gmail.com>
2015-12-07 20:37:59 -0500

    Misc. fixes; Ameneh: home page, cancel event (partial)

--- src/partial/home/home.js
+++ src/partial/home/home.js
@@ -1,13 +1,2 @@
 ﻿app.controller("HomeCtrl", function ($scope, $http) {
-    $scope.angular_hello = "Hello from Angular!";
-    var val = "Node";
-    $http.get("/api/hello/" + val)
-    .success(function (response) {
-        $scope.node_hello = response;
-    })
-    .error(function (data, status) {
-        $scope.node_hello = "call to node.js failed!";
-        console.log(data);
-        console.log(status);
-    });
 });
