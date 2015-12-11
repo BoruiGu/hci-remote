14cdf322ebc334db804b50c6a770f18fc67002e6
AmenehSh <am.shamekhi@gmail.com>
2015-12-11 18:20:21 -0500

    Borui's changes

--- src/partial/member_event/member_event.js
+++ src/partial/member_event/member_event.js
@@ -23,8 +23,8 @@
         $scope.rsvpinfo = [];
         User.userRsvpInfo($rootScope.username, function (response) {
             rsvped_event = response;
-            for (var idx in $scope.events) {
-                if (contains($scope.events[idx].id, rsvped_event)) {
+            for (var idx in $scope.filteredEvents) {
+                if (contains($scope.filteredEvents[idx].id, rsvped_event)) {
                     $scope.rsvpinfo.push(true);
                 } else {
                     $scope.rsvpinfo.push(false);
@@ -41,7 +41,7 @@
         var rsvp_info = {
             event_id: event_id,
             username: $rootScope.username,
-            message: comment + '; My Phone #: ' + phone
+            message: phone ? comment + '; My Phone #: ' + phone : comment
         }
         Event.rsvp(rsvp_info, function (response) {
             getUserRsvpInfo();
